0910051824f2ec5529ccabf490875bf78d656de3
Meronawit Dejene <150444100+Mer-World@users.noreply.github.com>
2025-08-15 11:55:40 +0300

    Create __manifest__.py

---
+++ addons/book_tracker/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Book Tracker",
+    "version": "1.0",
+    "category": "Productivity",
+    "summary": "Track books you own or want to read forerver",
+    "author": "Meronawit Dejene",
+    "depends": ["base"],
+    'data': [
+    'security/ir.model.access.csv',
+    'views/book_views.xml',
+    ],
+    "installable": True,
+    "application": True,
+}
